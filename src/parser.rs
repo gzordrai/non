1993f872678dc9e0e96515f57e030998cf4a5c3b
Francois Gibier <francois.gibier.etu@univ-lille.fr>
2025-11-05 23:43:21 +0100

    tokenizer done

--- src/parser.rs
+++ src/parser.rs
@@ -1,67 +1,114 @@
 use nom::{
     IResult, Needed, Parser,
-    bytes::complete::tag,
+    branch::alt,
+    bytes::complete::{take_while, take_while1},
     character::{
         char,
-        complete::{alpha1, newline},
+        complete::{alpha1, newline, space1},
     },
-    multi::{many, many1},
+    combinator::complete,
+    multi::{many, many0, many1},
 };
-use std::{collections::HashMap, str::FromStr};
+use std::{
+    collections::{HashMap, VecDeque},
+    str::FromStr,
+};
 
-fn parse_newline(r: &str) -> IResult<&str, &str> {
-    newline(r).and_then(|(r, _)| Ok((r, "")))
+fn parse_char_to_token(s: &str, c: char, token: Token) -> IResult<&str, Token> {
+    char(c).parse(s).map(|(rest, _)| (rest, token))
 }
 
 fn parse_identifier(s: &str) -> IResult<&str, Token> {
-    (alpha1, char(':'), parse_newline)
+    take_while1(|c: char| c.is_alphanumeric() || c == '_')
         .parse(s)
-        .map(|(rest, (id, _, _))| (rest, Token::Identifier(id.to_string())))
+        .map(|(rest, id)| (rest, Token::Identifier(id.to_string())))
 }
 
-fn parse_field(s: &str) -> IResult<&str, Token> {
-    (char('.'), alpha1, char(' '), alpha1)
-    .parse(s)
-    .map(|(rest, (_, name, _, value)| (rest, Token::Litteral(value))))
+fn parse_string_litteral(s: &str) -> IResult<&str, Token> {
+    (char('\''), take_while1(|c| c != '\''), char('\''))
+        .parse(s)
+        .map(|(rest, (_, s, _))| (rest, Token::Litteral(s.to_string())))
+}
+
+fn parse_dot(s: &str) -> IResult<&str, Token> {
+    parse_char_to_token(s, '.', Token::Dot)
+}
+
+fn parse_at(s: &str) -> IResult<&str, Token> {
+    parse_char_to_token(s, '@', Token::At)
+}
+
+fn parse_colon(s: &str) -> IResult<&str, Token> {
+    parse_char_to_token(s, ':', Token::Colon)
+}
+
+fn parse_whitespace(s: &str) -> IResult<&str, Token> {
+    space1(s).map(|(rest, _)| (rest, Token::Space))
+}
+
+fn parse_newline(s: &str) -> IResult<&str, Token> {
+    newline.parse(s).map(|(rest, _)| (rest, Token::Newline))
 }
 
 #[derive(Debug)]
 pub struct Tokenizer {
-    tokens: Vec<Token>,
+    pub tokens: Vec<Token>,
+}
+
+impl Tokenizer {
+    pub fn new(tokens: Vec<Token>) -> Self {
+        Self { tokens }
+    }
 }
 
 impl FromStr for Tokenizer {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut tokens = vec![];
-        let (mut rest, identifier) = parse_identifier(s).unwrap();
-        println!("rest: {rest}");
-        tokens.push(identifier);
+        let (rest, tokens) = many0(complete(alt((
+            parse_identifier,
+            parse_string_litteral,
+            parse_whitespace,
+            parse_dot,
+            parse_colon,
+            parse_at,
+            parse_newline,
+        ))))
+        .parse(s)
+        .map_err(|err| err.to_string())?;
 
-        rest = rest.trim();
-
-        let (rest, field) = parse_field(rest).unwrap();
-
-        println!("ok {:?}", tag("aa").parse("aa") as IResult<&str, &str>);
-
-        println!("{tokens:?}");
-
-        Ok(Tokenizer { tokens })
+        if !rest.is_empty() {
+            Err(format!("Error parsing file, rest: {rest}"))
+        } else {
+            Ok(Tokenizer::new(tokens))
+        }
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Token {
-    Litteral(String),
     Identifier(String),
-    Ref(String),
-    ObjRef(String, String),
+    Litteral(String),
+    Space,
+    Dot,
+    Colon,
+    At,
+    Newline,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 pub struct Non {
-    fields: HashMap<String, String>,
+    id: String,
+    fields: HashMap<String, FieldValue>,
+    parent: Option<Box<Non>>,
+}
+
+#[derive(Debug, Clone)]
+enum FieldValue {
+    Litteral(String),
+    Vec(Vec<FieldValue>),
+    FieldReference(String),
+    ObjRef(String, String),
 }
 
 impl Non {
@@ -70,8 +117,17 @@ impl Non {
     }
 }
 
-// impl From<Path> for Non {
-//     fn from(value: Path) -> Self {
-//         todo!()
-//     }
-// }
+#[derive(Debug)]
+pub struct Lexer {
+    pub tokens: VecDeque<Token>,
+    pub noms: HashMap<String, Non>,
+}
+
+impl Lexer {
+    pub fn new(tokenizer: Tokenizer) -> Self {
+        Self {
+            tokens: VecDeque::from(tokenizer.tokens),
+            noms: HashMap::new(),
+        }
+    }
+}
