30dc8aadce1658a39f7444cd52e5ee893e6b6f42
Thibault Tisserand <40698142+gzordrai@users.noreply.github.com>
2025-11-10 15:22:22 +0100

    refactor: use custom errors instead of unwrap

--- src/error.rs
+++ src/error.rs
@@ -2,13 +2,25 @@ use thiserror::Error;
 
 use crate::token::TokenKind;
 
-pub type Result<T> = std::result::Result<T, LexerError>;
+pub type Result<T> = std::result::Result<T, NonError>;
 
 #[derive(Debug, Error)]
-pub enum LexerError {
+pub enum NonError {
+    #[error("Tokenize error")]
+    TokenizeFailed,
+
     #[error("Identifier and litterals tokens need a value to be instanciated.")]
     MissingTokenValue,
 
     #[error("Token kind {0} cannot have a value.")]
     InvalidTokenKind(TokenKind),
+
+    #[error("IO error")]
+    IoError(#[from] std::io::Error),
+
+    #[error("Json serialization error")]
+    JsonSerializationError(#[from] serde_json::Error),
+
+    #[error("Json serialization error")]
+    YamlSerializationError(#[from] serde_yaml::Error),
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use std::{fs::File, io::Read, path::Path};
+use std::{
+    fs::File,
+    io::{Read, Write},
+    path::Path,
+};
 
 use clap::Parser;
 
 use crate::{
     args::{Args, OutputFormat},
+    error::Result,
     lexer::NonLexer,
     parser::NonParser,
 };
@@ -15,15 +20,15 @@ mod non;
 mod parser;
 mod token;
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
     let path = Path::new(&args.path);
 
     if path.exists() {
-        let mut file = File::open(path).unwrap();
+        let mut file = File::open(path)?;
         let mut buf = String::default();
 
-        file.read_to_string(&mut buf).unwrap();
+        file.read_to_string(&mut buf)?;
 
         let lexer = NonLexer::new(&buf);
         let mut parser = NonParser::new(lexer);
@@ -31,15 +36,24 @@ fn main() {
         parser.parse();
 
         let non = parser.get_non_by_id("student").unwrap();
-
-        if let Some(format) = &args.format {
-            let content = match format {
-                OutputFormat::Json => serde_json::to_string_pretty(&*non).unwrap(),
-                OutputFormat::Yaml => serde_yaml::to_string(&*non).unwrap(),
+        let content = if let Some(format) = &args.format {
+            match format {
+                OutputFormat::Json => serde_json::to_string_pretty(&*non)?,
+                OutputFormat::Yaml => serde_yaml::to_string(&*non)?,
                 OutputFormat::Non => parser.serialize(),
-            };
+            }
+        } else {
+            parser.serialize()
+        };
 
+        if let Some(output) = &args.output {
+            let mut f = File::create(output)?;
+
+            f.write_all(&content.into_bytes())?;
+        } else {
             println!("{}", content);
         }
     }
+
+    Ok(())
 }
