322d5b754bcb964ddd253fbf3fe2f63f9fd7f53f
Thibault Tisserand <40698142+gzordrai@users.noreply.github.com>
2025-11-16 16:59:42 +0100

    refactor: start to refactor for the new parser

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,36 @@ use std::{fs::File, io::Read, path::Path};
 
 use clap::Parser;
 
-use crate::{args::Args, lexer::NonLexer, nds::NonDefs, parser::NonParser};
+// use crate::{args::Args, lexer::NonLexer, nds::NonDefs, parser::NonParser};
+use crate::{args::Args, error::Result, lexer::NonLexer, parser::NonParser};
 
 mod args;
 mod error;
 mod lexer;
-mod nds;
+// mod nds;
 mod non;
 mod parser;
-mod token;
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
     let path = Path::new(&args.path);
 
     if path.exists() {
-        let mut file = File::open(path).unwrap();
+        let mut file = File::open(path)?;
         let mut buf = String::default();
 
-        file.read_to_string(&mut buf).unwrap();
+        file.read_to_string(&mut buf)?;
+
         let lexer = NonLexer::new(&buf);
         let mut parser = NonParser::new(lexer);
 
-        parser.parse();
-        let non_defs = NonDefs::builder()
-            .format(args.format)
-            .nons(parser.nons)
-            .flat(args.flat)
-            .build();
+        parser.parse()?;
+
+        // let non_defs = NonDefs::builder()
+        //     .format(args.format)
+        //     .nons(parser.nons)
+        //     .flat(args.flat)
+        //     .build();
 
         // let content = non_defs.serialize();
         // println!("{}", content);
@@ -38,9 +40,11 @@ fn main() {
         // let bob = non_defs.at("bob").unwrap();
         // println!("alice.mail {}", alice.get("mail").unwrap());
 
-        let b = non_defs.at("b").unwrap();
-        let c = non_defs.at("c").unwrap();
-        let union = b.union(c);
-        println!("union : {:?}", union);
+        // let b = non_defs.at("b").unwrap();
+        // let c = non_defs.at("c").unwrap();
+        // let union = b.union(c);
+        // println!("union : {:?}", union);
     }
+
+    Ok(())
 }
--- src/non.rs
+++ src/non.rs
@@ -1,273 +1,229 @@
-use std::{
-    cell::{Ref, RefCell},
-    collections::{HashMap, HashSet},
-    rc::Rc,
-};
+use crate::parser::Field;
 
-use serde::Serialize;
-
-#[derive(Debug, Default, Clone, Serialize, PartialEq)]
+#[derive(Debug, Default, Clone)]
 pub struct Non {
-    id: String,
-    fields: HashMap<String, FieldValue>,
-    pub parents: Vec<Rc<RefCell<Non>>>,
+    pub name: String,
+    pub parents: Vec<String>,
+    pub fields: Vec<Field>,
 }
 
 impl Non {
-    pub fn new(
-        id: String,
-        fields: HashMap<String, FieldValue>,
-        parents: Vec<Rc<RefCell<Non>>>,
-    ) -> Self {
+    pub fn new(name: String, parents: Vec<String>, fields: Vec<Field>) -> Self {
         Self {
-            id,
-            fields,
+            name,
             parents,
-        }
-    }
-
-    pub fn from_id(id: String) -> Self {
-        let fields = HashMap::new();
-        Non {
-            id,
             fields,
-            ..Default::default()
         }
     }
 
-    pub fn id(&self) -> String {
-        self.id.clone()
+    pub fn add_field(&mut self, field: Field) {
+        self.fields.push(field);
     }
 
-    pub fn get(&self, field_name: &str) -> Option<String> {
-        self.fields()
-            .get(field_name)
-            .map(|field| self.resolve_field(field.clone()))
-    }
+    // pub fn from_id(id: String) -> Self {
+    //     let fields = HashMap::new();
+    //     Non {
+    //         id,
+    //         fields,
+    //         ..Default::default()
+    //     }
+    // }
 
-    pub fn add_field(&mut self, name: String, value: FieldValue) {
-        self.fields.insert(name, value);
-    }
+    // pub fn id(&self) -> String {
+    //     self.id.clone()
+    // }
 
-    pub fn resolve_field(&self, field_value: FieldValue) -> String {
-        let mut str = String::new();
-        match field_value {
-            FieldValue::Litteral(v) => str.push_str(&v),
-            FieldValue::Vec(field_values) => {
-                for field_value in field_values {
-                    str.push_str(self.resolve_field(field_value).as_str());
-                }
-            }
-            FieldValue::FieldReference(reference) => {
-                if reference == "id" {
-                    str.push_str(&self.id());
-                } else {
-                    str.push_str(self.get(&reference).unwrap().as_str())
-                }
-            }
-            FieldValue::ObjRef(non, field_name) => {
-                str.push_str(non.borrow().get(&field_name).unwrap().as_str())
-            }
-        }
-        str
-    }
+    // pub fn get(&self, field_name: &str) -> Option<String> {
+    //     self.fields()
+    //         .get(field_name)
+    //         .map(|field| self.resolve_field(field.clone()))
+    // }
 
-    pub fn union(&self, other: Ref<'_, Non>) -> Result<Non, String> {
-        let fields = self.fields();
-        let other_fields = other.fields();
+    // pub fn add_field(&mut self, name: String, value: FieldValue) {
+    //     self.fields.insert(name, value);
+    // }
 
-        for (name, value) in &fields {
-            if let Some(other_value) = other_fields.get(name) {
-                if other_value != value {
-                    return Err(format!("Duplicated field '{}' without same value.", name));
-                }
-            }
-        }
+    // pub fn resolve_field(&self, field_value: FieldValue) -> String {
+    //     let mut str = String::new();
+    //     match field_value {
+    //         FieldValue::Litteral(v) => str.push_str(&v),
+    //         FieldValue::Vec(field_values) => {
+    //             for field_value in field_values {
+    //                 str.push_str(self.resolve_field(field_value).as_str());
+    //             }
+    //         }
+    //         FieldValue::FieldReference(reference) => {
+    //             if reference == "id" {
+    //                 str.push_str(&self.id());
+    //             } else {
+    //                 str.push_str(self.get(&reference).unwrap().as_str())
+    //             }
+    //         }
+    //         FieldValue::ObjRef(non, field_name) => {
+    //             str.push_str(non.borrow().get(&field_name).unwrap().as_str())
+    //         }
+    //     }
+    //     str
+    // }
 
-        for (name, value) in &other_fields {
-            if let Some(other_value) = fields.get(name) {
-                if *other_value != *value {
-                    return Err(format!("Duplicated field '{}' without same value.", name));
-                }
-            }
-        }
+    // pub fn union(&self, other: Ref<'_, Non>) -> Result<Non, String> {
+    //     let fields = self.fields();
+    //     let other_fields = other.fields();
 
-        let mut union_fields = self.fields();
-        union_fields.extend(other_fields);
+    //     for (name, value) in &fields {
+    //         if let Some(other_value) = other_fields.get(name) {
+    //             if other_value != value {
+    //                 return Err(format!("Duplicated field '{}' without same value.", name));
+    //             }
+    //         }
+    //     }
 
-        let mut parents = self.parents.iter().cloned().collect::<Vec<_>>();
-        parents.extend(other.parents.iter().cloned());
+    //     for (name, value) in &other_fields {
+    //         if let Some(other_value) = fields.get(name) {
+    //             if *other_value != *value {
+    //                 return Err(format!("Duplicated field '{}' without same value.", name));
+    //             }
+    //         }
+    //     }
 
-        // filter parents to avoid duplications
-        let mut seen = HashSet::new();
-        parents.retain(|p| {
-            let ptr = Rc::as_ptr(p);
-            seen.insert(ptr)
-        });
+    //     let mut union_fields = self.fields();
+    //     union_fields.extend(other_fields);
 
-        Ok(Non::new(self.id(), union_fields, parents))
-    }
+    //     let mut parents = self.parents.iter().cloned().collect::<Vec<_>>();
+    //     parents.extend(other.parents.iter().cloned());
 
-    pub fn serialize_non(&self, flat: bool) -> String {
-        let mut str = String::new();
+    //     // filter parents to avoid duplications
+    //     let mut seen = HashSet::new();
+    //     parents.retain(|p| {
+    //         let ptr = Rc::as_ptr(p);
+    //         seen.insert(ptr)
+    //     });
 
-        str.push_str(&self.id().to_string());
-        str.push(':');
+    //     Ok(Non::new(self.id(), union_fields, parents))
+    // }
 
-        if !flat {
-            for parent_ref in &self.parents {
-                let parent = parent_ref.borrow();
-                str.push(' ');
-                str.push_str(&parent.id());
-            }
-        }
+    // pub fn serialize_non(&self, flat: bool) -> String {
+    //     let mut str = String::new();
 
-        str.push('\n');
+    //     str.push_str(&self.id().to_string());
+    //     str.push(':');
 
-        let fields = if flat { &self.fields() } else { &self.fields };
+    //     if !flat {
+    //         for parent_ref in &self.parents {
+    //             let parent = parent_ref.borrow();
+    //             str.push(' ');
+    //             str.push_str(&parent.id());
+    //         }
+    //     }
 
-        for (key, value) in fields {
-            let serialized_field_value = if flat {
-                self.resolve_field(value.clone())
-            } else {
-                value.to_string()
-            };
-            str.push_str(&format!(".{} {}\n", key, serialized_field_value));
-        }
+    //     str.push('\n');
 
-        str
-    }
+    //     let fields = if flat { &self.fields() } else { &self.fields };
 
-    pub fn serialize_json(&self, flat: bool) -> String {
-        let mut str = String::new();
+    //     for (key, value) in fields {
+    //         let serialized_field_value = if flat {
+    //             self.resolve_field(value.clone())
+    //         } else {
+    //             value.to_string()
+    //         };
+    //         str.push_str(&format!(".{} {}\n", key, serialized_field_value));
+    //     }
 
-        str.push_str("{\n");
-        str.push_str("\t\"id\": \"");
-        str.push_str(&self.id());
-        str.push_str("\"");
+    //     str
+    // }
 
-        let fields = if flat { &self.fields() } else { &self.fields };
+    // pub fn serialize_json(&self, flat: bool) -> String {
+    //     let mut str = String::new();
 
-        if !(fields.is_empty() && self.parents.is_empty()) {
-            str.push(',');
-        }
+    //     str.push_str("{\n");
+    //     str.push_str("\t\"id\": \"");
+    //     str.push_str(&self.id());
+    //     str.push_str("\"");
 
-        if !flat && !self.parents.is_empty() {
-            str.push_str("\n\t\"parents\": [\n");
-            let parent_str = self
-                .parents
-                .iter()
-                .map(|parent| format!("\t\t\"{}\"", parent.borrow().id()))
-                .collect::<Vec<String>>()
-                .join(",\n");
-            str.push_str(&parent_str);
-            str.push_str("\n\t]");
-            if !fields.is_empty() {
-                str.push(',');
-            }
-        }
+    //     let fields = if flat { &self.fields() } else { &self.fields };
 
-        if !fields.is_empty() {
-            str.push_str("\n\t\"fields\":\n\t{\n");
+    //     if !(fields.is_empty() && self.parents.is_empty()) {
+    //         str.push(',');
+    //     }
 
-            let fields_str = fields
-                .iter()
-                .map(|(field_name, value)| {
-                    if flat {
-                        format!(
-                            "\t\t\"{}\": \"{}\"",
-                            field_name,
-                            self.resolve_field(value.clone())
-                        )
-                    } else {
-                        format!("\t\t\"{}\": \"{}\"", field_name, value.to_string())
-                    }
-                })
-                .collect::<Vec<_>>()
-                .join(",\n");
-            str.push_str(&fields_str);
-            str.push_str("\n\t}");
-        }
+    //     if !flat && !self.parents.is_empty() {
+    //         str.push_str("\n\t\"parents\": [\n");
+    //         let parent_str = self
+    //             .parents
+    //             .iter()
+    //             .map(|parent| format!("\t\t\"{}\"", parent.borrow().id()))
+    //             .collect::<Vec<String>>()
+    //             .join(",\n");
+    //         str.push_str(&parent_str);
+    //         str.push_str("\n\t]");
+    //         if !fields.is_empty() {
+    //             str.push(',');
+    //         }
+    //     }
 
-        str.push_str("\n}");
-        str
-    }
+    //     if !fields.is_empty() {
+    //         str.push_str("\n\t\"fields\":\n\t{\n");
 
-    pub fn serialize_yaml(&self, flat: bool) -> String {
-        let mut str = String::new();
+    //         let fields_str = fields
+    //             .iter()
+    //             .map(|(field_name, value)| {
+    //                 if flat {
+    //                     format!(
+    //                         "\t\t\"{}\": \"{}\"",
+    //                         field_name,
+    //                         self.resolve_field(value.clone())
+    //                     )
+    //                 } else {
+    //                     format!("\t\t\"{}\": \"{}\"", field_name, value.to_string())
+    //                 }
+    //             })
+    //             .collect::<Vec<_>>()
+    //             .join(",\n");
+    //         str.push_str(&fields_str);
+    //         str.push_str("\n\t}");
+    //     }
 
-        str.push_str(&self.id());
-        str.push_str(":");
+    //     str.push_str("\n}");
+    //     str
+    // }
 
-        if !self.parents.is_empty() {
-            str.push_str("\n\tparents:\n\t");
-            let parent_str = self
-                .parents
-                .iter()
-                .map(|parent| format!("  - {}", parent.borrow().id()))
-                .collect::<Vec<String>>()
-                .join("\n\t");
-            str.push_str(&parent_str);
-        }
+    // pub fn serialize_yaml(&self, flat: bool) -> String {
+    //     let mut str = String::new();
 
-        if !self.fields.is_empty() {
-            str.push('\n');
-        }
+    //     str.push_str(&self.id());
+    //     str.push_str(":");
 
-        let fields = if flat { &self.fields() } else { &self.fields };
+    //     if !self.parents.is_empty() {
+    //         str.push_str("\n\tparents:\n\t");
+    //         let parent_str = self
+    //             .parents
+    //             .iter()
+    //             .map(|parent| format!("  - {}", parent.borrow().id()))
+    //             .collect::<Vec<String>>()
+    //             .join("\n\t");
+    //         str.push_str(&parent_str);
+    //     }
 
-        let fields_str = fields
-            .iter()
-            .map(|(field_name, value)| {
-                if flat {
-                    format!("\t{}: {}", field_name, self.resolve_field(value.clone()))
-                } else {
-                    format!("\t{}: {}", field_name, value.to_string())
-                }
-            })
-            .collect::<Vec<_>>()
-            .join("\n");
-        str.push_str(&fields_str);
+    //     if !self.fields.is_empty() {
+    //         str.push('\n');
+    //     }
 
-        str.push_str("\n");
-        str
-    }
+    //     let fields = if flat { &self.fields() } else { &self.fields };
 
-    fn fields(&self) -> HashMap<String, FieldValue> {
-        let mut map = HashMap::new();
+    //     let fields_str = fields
+    //         .iter()
+    //         .map(|(field_name, value)| {
+    //             if flat {
+    //                 format!("\t{}: {}", field_name, self.resolve_field(value.clone()))
+    //             } else {
+    //                 format!("\t{}: {}", field_name, value.to_string())
+    //             }
+    //         })
+    //         .collect::<Vec<_>>()
+    //         .join("\n");
+    //     str.push_str(&fields_str);
 
-        for parent_ref in &self.parents {
-            map.extend(parent_ref.borrow().fields().into_iter());
-        }
-
-        map.extend(self.fields.clone().into_iter());
-        map
-    }
-}
-
-#[derive(Debug, Clone, Serialize, PartialEq)]
-pub enum FieldValue {
-    Litteral(String),
-    Vec(Vec<FieldValue>),
-    FieldReference(String),
-    ObjRef(Rc<RefCell<Non>>, String),
-}
-
-impl FieldValue {
-    pub fn to_string(&self) -> String {
-        match self {
-            FieldValue::Litteral(str) => str.clone(),
-            FieldValue::Vec(field_values) => field_values
-                .iter()
-                .map(|f| f.to_string())
-                .collect::<Vec<_>>()
-                .join(" "),
-            FieldValue::FieldReference(reference) => {
-                if reference == "id" { "@" } else { reference }.to_string()
-            }
-            FieldValue::ObjRef(reference, field) => {
-                format!("{}.{}", reference.borrow().id(), field.clone())
-            }
-        }
-    }
+    //     str.push_str("\n");
+    //     str
+    // }
 }
